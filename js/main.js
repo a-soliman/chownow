0dd53951cceadcbea10f99c5b7bd20854218dfb3
Ahmed Soliman <ahmedanwar@mail.com>
2016-10-25 18:55:52 -0700

    coded the third month

--- js/main.js
+++ js/main.js
@@ -13,11 +13,22 @@ $(document).ready(function() {
 		$(this).find("h2").addClass("selected")
 		event.preventDefault();
 	})
+
 	$("#monthTwo").click(function() {
 		$(".expect-text").css("display", "none")
 			secondMonthContent.fadeIn(1000)
 
 			$("#monthOne, #monthThree").find("h2").removeClass("selected")
+			$(this).find("h2").addClass("selected")
+
+		event.preventDefault();
+	})
+
+	$("#monthThree").click(function() {
+		$(".expect-text").css("display", "none")
+			thirdMonthContent.fadeIn(1000)
+
+			$("#monthOne, #monthTwo").find("h2").removeClass("selected")
 			$(this).find("h2").addClass("selected")
 
 		event.preventDefault();
