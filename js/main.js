537ec9767bb8c83646574610ccc590b05d49e4ae
Ahmed Soliman <ahmedanwar@mail.com>
2016-10-25 18:54:18 -0700

    coded the first month

--- js/main.js
+++ js/main.js
@@ -4,4 +4,13 @@ $(document).ready(function() {
 	var thirdMonthContent = $(".thirdMonth")
 	
 	$("#monthOne").find("h2").addClass("selected")
+
+	$("#monthOne").click(function(event) {
+		$(".expect-text").css("display", "none")
+		firstMonthContent.fadeIn(1000)
+
+		$("#monthTwo, #monthThree").find("h2").removeClass("selected")
+		$(this).find("h2").addClass("selected")
+		event.preventDefault();
+	})
 })
